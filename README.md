c00d9801926fcbcb8230bc89cbc1691037e877ee
ppoliani <ppoliani@gmail.com>
2022-11-24 22:18:14 +0200

    feat: add tailwind css

---
+++ README.md
@@ -0,0 +1,8 @@
+Integrate Tailwindcss and Tailwind components
+===
+
+1. `npx tailwindcss -o ./tailwind.css`
+
+2. Include the css file in the index.html
+
+`<link data-trunk href="./tailwind.css" rel="css" />`
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.rs',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ]
+}
